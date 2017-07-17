9c0ddc78100679a9ee7f5353cf298c41e5af1cbd
Z-liu <liuzhao@yeah.net>
2017-07-17 16:47:25 +0800

    20170717

--- data_collector/views.py
+++ data_collector/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.views.generic import TemplateView
-from data_collector.models import DataPoint
+from django.views.generic import TemplateView, ListView, UpdateView, \
+    CreateView, DeleteView
+from data_collector.models import DataPoint, Alert
+from django.core.urlresolvers import reverse
 
 
 class StatusView(TemplateView):
@@ -10,13 +9,62 @@ class StatusView(TemplateView):
 
     def get_context_data(self, **kwargs):
         ctx = super(StatusView, self).get_context_data(**kwargs)
+        alerts = Alert.objects.filter(is_active=True)
         nodes_and_data_types = DataPoint.objects.all().values('node_name', 'data_type').distinct()
         status_data_dict = dict()
         for nodes_and_data_type_pair in nodes_and_data_types:
             node_name = nodes_and_data_type_pair['node_name']
             data_type = nodes_and_data_type_pair['data_type']
+            latest_data_point = DataPoint.objects.filter(node_name=node_name, data_type=data_type).latest('datetime')
+            latest_data_point.has_alert = self.does_have_alert(latest_data_point, alerts)
             data_point_map = status_data_dict.setdefault(node_name, dict())
-            data_point_map[data_type] = DataPoint.objects.filter(node_name=node_name, data_type=data_type).latest(
-                'datetime')
+            data_point_map[data_type] = latest_data_point
         ctx['status_data_dict'] = status_data_dict
         return ctx
+
+    def does_have_alert(self, data_point, alerts):
+        for alert in alerts:
+            if alert.node_name and data_point.node_name != alert.node_name:
+                continue
+            if alert.data_type != data_point.data_type:
+                continue
+            if alert.min_value is not None and data_point.data_value < alert.min_value:
+                return True
+            if alert.max_value is not None and data_point.data_value > alert.max_value:
+                return True
+            return False
+
+
+class AlertListView(ListView):
+    template_name = 'alerts_list.html'
+    model = Alert
+
+
+class NewAlertView(CreateView):
+    template_name = 'create_or_update_alert.html'
+    model = Alert
+    fields = [
+        'data_type', 'min_value', 'max_value', 'node_name', 'is_active'
+    ]
+
+    def get_success_url(self):
+        return reverse('alerts-list')
+
+
+class EditAlertView(UpdateView):
+    template_name = 'create_or_update_alert.html'
+    model = Alert
+    fields = [
+        'data_type', 'min_value', 'max_value', 'node_name', 'is_active'
+    ]
+
+    def get_success_url(self):
+        return reverse('alerts-list')
+
+
+class DeleteAlertView(DeleteView):
+    template_name = 'delete_alert.html'
+    model = Alert
+
+    def get_success_url(self):
+        return reverse('alerts-list')
--- djagios/urls.py
+++ djagios/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from data_collector.views import StatusView
+from data_collector.views import StatusView, AlertListView, NewAlertView, \
+    EditAlertView, DeleteAlertView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', StatusView.as_view(), name='status'),
+    url(r'^alerts/$', AlertListView.as_view(), name='alerts-list'),
+    url(r'^alerts/new/$', NewAlertView.as_view(), name='alerts-new'),
+    url(r'^alerts/(?P<pk>\d+)/edit/$', EditAlertView.as_view(),
+        name='alerts-edit'),
+    url(r'^alerts/(?P<pk>\d+)/delete/$', DeleteAlertView.as_view(),
+        name='alerts-delete'),
 ]
